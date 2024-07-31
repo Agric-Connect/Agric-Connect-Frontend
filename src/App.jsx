0dda5c97f7aab6432951f818f31a6c38596e09eb
AJ-Appiah <ericapph@gmail.com>
2024-07-31 22:30:01 +0000

    File structure setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+
+const router = createBrowserRouter([
+
+]);
+
+function App() {
+  return (<RouterProvider router={router} /> );
+}
+
+export default App
